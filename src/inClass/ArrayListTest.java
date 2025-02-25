68d5b9e4d77c73870c529718dc5e050e9580ac9d
syeda <Ameerasyed90@gmail.com>
2025-02-25 16:58:45 -0600

    Update inClass

---
+++ src/inClass/ArrayListTest.java
@@ -0,0 +1,53 @@
+package inClass;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    private TheArrayList<Integer> list;
+    @BeforeEach
+    void setUp() {
+        list = new TheArrayList<>();
+    }
+
+    @Test
+    @DisplayName("Testing ArrayList size")
+    void size(){
+        final int numTests = 10_000;
+        Assertions.assertEquals(0, list.size());
+        Assertions.assertTrue(list.add(1));
+        Assertions.assertEquals(1, list.size());
+        list = new TheArrayList<>();
+        for (int i = 0; i < numTests; ++i){
+            Assertions.assertEquals(i, list.size());
+            Assertions.assertTrue(list.add(i));
+        }
+    }
+    @Test
+    @DisplayName("Testing Contains")
+    void contains(){
+        //boundary Conditions
+        //1. list is empty - false
+        //2. list not empty but item not there - false
+        //3. list not empty but item is there - true
+        //4 list not empty but multiple copies - true
+        //5. list has 1 item - our item - true
+        //6. list has 1 item - not our item - false;
+        //7. list has many items - ours is last - true;
+        list = new TheArrayList<>();
+        Assertions.assertFalse(list.contains(1));
+        list.add(0);
+        Assertions.assertFalse(list.contains(1));
+        Assertions.assertFalse(list.contains(0));
+        list.add(1);
+        list.add(2);
+        Assertions.assertTrue(list.contains(1));
+        Assertions.assertTrue(list.contains(2));
+        Assertions.assertFalse(list.contains(3));
+
+    }
+}
---
+++ src/inClass/TheLinkedList.java
@@ -0,0 +1,169 @@
+package inClass;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class TheLinkedList<E> implements List<E> {
+    private Node<E> head;
+    private int size;
+
+    public TheLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        // O(n)
+        if (head == null) {
+            head = new Node<>(e);
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(e);
+        }
+        size++;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return List.of();
+    }
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+        private Node(E element) {
+            this(element, null);
+        }
+        private Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public String toString() {
+            return element.toString();
+        }
+    }
+
+}
